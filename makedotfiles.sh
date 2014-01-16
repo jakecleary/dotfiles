749795891b76789530a80d246eb0d5d133f2ebe0
clear-y <account@jakecleary.net>
2014-01-16 21:58:43 +0000

    Added git_status alias

--- makedotfiles.sh
+++ makedotfiles.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc gitconfig"    # list of files/folders to symlink in homedir
+files="bashrc bash_aliases gitconfig"    # list of files/folders to symlink in homedir
 
 ##########
 
