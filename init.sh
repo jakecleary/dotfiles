f131e43888c2c17fd2c1f158f5b7449740d6e4d9
clear-y <shout@jakecleary.net>
2014-06-28 22:05:21 +0100

    Wrong file name

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ############################
-# .make.sh
+# init.sh
 # This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
 ############################
 
